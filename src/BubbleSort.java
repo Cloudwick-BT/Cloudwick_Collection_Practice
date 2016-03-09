c6000485387050dba7fdfc77130d9c5c6560042f
bhavintandel <bhautandel@yahoo.com.com>
2016-03-09 10:29:00 +0000

    Bubble sort for integer

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,24 +6,37 @@ import java.util.*;
  */
 
 public class BubbleSort {
-	public static void main(String[] args){
-		String[] strArray = new String[] { "egg", "ball", "frog", "Abascus", "Zebra", "keyboard"};
-		ArrayList <String>  arraylist = new ArrayList<String>();
-		//arraylist.addAll(Arrays.asList(strArray));
-		for(String str : strArray){
-			arraylist.add(str);
-		}
-
+	
+	public static void bubbleSort(ArrayList<Integer> arraylist ){
 		for(int i = 0; i < arraylist.size(); i++){
-			for (int j = i; j < arraylist.size(); j++){
-				if(((arraylist.get(i)).toLowerCase()).compareTo((arraylist.get(j)).toLowerCase()) > 0){
-					String temp = arraylist.get(i);
+			for (int j = i+1; j < arraylist.size(); j++){
+				if(((arraylist.get(i))).compareTo((arraylist.get(j))) > 0){
+					int temp = arraylist.get(i);
 					arraylist.set(i, arraylist.get(j));
 					arraylist.set(j, temp);
 				}
 			}
-			System.out.println(arraylist.get(i));
+			//System.out.println(arraylist.get(i));
 		}
+	}
+	
+	public static void main(String[] args){
+		int[] rndNum = new int[10000];
+		
+		for (int i=0; i<rndNum.length; i++){
+			rndNum[i] = new Random().nextInt(Integer.MAX_VALUE);
+		}
+		ArrayList <Integer>  arraylist = new ArrayList<Integer>();
+
+		for(Integer str : rndNum){
+			arraylist.add(str);
+		}
+		long startTime = System.currentTimeMillis();
+
+		// bubble sort
+		bubbleSort(arraylist);
+		long stopTime = System.currentTimeMillis();
+		System.out.println("Total time : " + (stopTime - startTime));
 	
 	}
 
